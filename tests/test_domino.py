3f4e2903ec21f03c4189987ad904cb594d5053c1
Falimampianina <falimampianina@gmail.com>
2024-07-21 10:03:09 +0200

    Add unittest for Domino

---
+++ tests/test_domino.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+from data_structures.playables.domino import Domino
+
+
+class TestDomino(TestCase):
+    def test_domino(self):
+        with self.assertRaises(ValueError):
+            Domino(7, 7)
+
+    def test_equivalence(self):
+        self.assertEqual(Domino(1, 3), Domino(3, 1))
+
+    def test_reverse(self):
+        self.assertEqual(str(Domino(1, 2)),str(Domino(2, 1).reverse()))
+
