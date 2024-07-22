16bcfcbdb88a93c9460187e0d843cf19f6968a90
Falimampianina <falimampianina@gmail.com>
2024-07-22 21:03:27 +0200

    Add play method for AIPlayer

--- data_structures/players/ai_player.py
+++ data_structures/players/ai_player.py
@@ -1,4 +1,7 @@
+import random
+
 from data_structures.playables.domino import Domino
+from data_structures.playables.domino_snake import DominoSnake
 from data_structures.players.player import Player
 
 
@@ -14,3 +17,19 @@ class AIPlayer(Player):
     @level.setter
     def level(self, value: int):
         self._level = value
+
+    def _ai_make_random_legal_move(self, snake: DominoSnake):
+        playable_pieces = []
+        for domino in self.dominoes:
+            if snake.check_if_domino_can_be_placed(domino):
+                playable_pieces.append(domino)
+        chosen_piece = random.choice(playable_pieces)
+        snake.place_domino(chosen_piece)
+        self.dominoes.remove(chosen_piece)
+
+    def play(self, snake: DominoSnake):
+        if self.useless_hand(snake):
+            return
+        else:
+            if self.level == 0:
+                self._ai_make_random_legal_move(snake)
